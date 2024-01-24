95b9455b4ecf7a9e80baddc8297c485283a02436
KEVIN CHAU <kevinch@udel.edu>
2024-01-24 02:00:55 -0500

    Added NavBar styling

--- src/modules/NavBar.js
+++ src/modules/NavBar.js
@@ -1,21 +1,32 @@
+function createButtons(){
+        //Button 1
+        let homeButton = document.createElement('button');
+        homeButton.textContent = 'Home';
+        homeButton.setAttribute('class', 'nav-buttons')
+        //Button2
+        let menuButton = document.createElement('button');
+        menuButton.textContent = 'Menu'
+        menuButton.setAttribute('class', 'nav-buttons')
+        //Button 3
+        let contactButton = document.createElement('button');
+        contactButton.textContent = 'Contact';
+        contactButton.setAttribute('class', 'nav-buttons')
+
+        let navRight = document.createElement('div');
+        navRight.setAttribute('class', 'nav-right');
+        navRight.appendChild(homeButton);
+        navRight.appendChild(menuButton);
+        navRight.appendChild(contactButton);
+
+        return navRight;
+}
+
 export function NavBar(){
-    //Button 1
-    let homeButton = document.createElement('button');
-    //Button2
-    let menuButton = document.createElement('button');
-    //Button 3
-    let contactButton = document.createElement('button');
-
-    let navRight = document.createElement('div');
-    navRight.setAttribute('class', 'nav-right');
-    navRight.appendChild(homeButton);
-    navRight.appendChild(menuButton);
-    navRight.appendChild(contactButton);
-
-
     let NavBar = document.createElement('div');
-    NavBar.textContent = "Pal Cafe";
-    NavBar.appendChild(navRight);
+    let h1 = document.createElement('h1');
+    h1.textContent = "Pal Cafe";
+    NavBar.appendChild(h1);
+    NavBar.appendChild(createButtons());
     NavBar.setAttribute('class', 'NavBar');
     return NavBar;
 }
